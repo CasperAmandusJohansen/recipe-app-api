83861f013d27759502a34b226d7e24f06262fa81
unknown <casper.johansen404@gmail.com>
2020-04-26 11:09:15 +0200

    dded user mod API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
+from django.conf import settings
+
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
+        abstract = 'rest_framework.authtoken' not in settings.INSTALLED_APPS
         fields = ('email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5 }}
 
@@ -18,6 +21,17 @@ class UserSerializer(serializers.ModelSerializer):
         Create a new user with encrypted password and return it
         """
         return get_user_model().objects.create_user(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the use auth object"""
