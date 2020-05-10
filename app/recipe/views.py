22d299a332f0ce2803a0e20aa7dabab715583377
unknown <casper.johansen404@gmail.com>
2020-05-10 23:40:58 +0200

    dded filtering

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,8 +15,17 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        """Return objects for current user"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new ingredient"""
@@ -42,10 +51,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
 
     def _params_to_ints(self, qs):
-        """Convert a list of string id's to a list of integers"""
+        """Convert a list of string IDs to a list of integers"""
         return [int(str_id) for str_id in qs.split(',')]
-
-
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
@@ -60,7 +67,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
         return queryset.filter(user=self.request.user)
-
+    
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
